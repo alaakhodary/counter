dca1477b427f57a113328b7a86addce7641dfaf0
Alaa Alkhodary <alaaalkhodary2000@gmail.com>
2022-12-20 11:07:34 +0200

    counter project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Counter from "../src/Component/Counter";
 function App() {
   return (
     <div className="App">
-      <Counter initial={5} />
       <Counter />
-      <Counter />
+      <Counter initial={10} />
+      <Counter initial={15} />
     </div>
   );
 }
